5a5556f44be8b770a8564fc07ce0fbdf5db3e60f
Luke Harper <lukeharper@live.ca>
2015-02-15 12:08:43 -0500

    Added a Board class
    Added the Board Stub

---
+++ CommonLibrary/board.h
@@ -0,0 +1,11 @@
+#ifndef BOARD_H
+#define BOARD_H
+#include "commonlibrary_global.h"
+
+//Abstract class, extend this with all different Character types
+class  COMMONLIBRARY_EXPORT Board{
+public:
+	Board();
+};
+
+#endif // BOARD_H
