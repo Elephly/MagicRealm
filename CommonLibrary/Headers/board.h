9b2f5ee444691cfce1ee297b0f7881299bff15cc
Luke Harper <lukeharper@live.ca>
2015-02-15 12:56:10 -0500

    moved to source and header folders
    tadah

--- CommonLibrary/Headers/board.h
+++ CommonLibrary/Headers/board.h
@@ -1,11 +1,15 @@
 #ifndef BOARD_H
 #define BOARD_H
 #include "commonlibrary_global.h"
+#include "tile.h"
+#include <unordered_map>
 
 //Abstract class, extend this with all different Character types
 class  COMMONLIBRARY_EXPORT Board{
 public:
 	Board();
+private:
+    std::unordered_map<Tile*, Tile[]> tiles;
 };
 
 #endif // BOARD_H
