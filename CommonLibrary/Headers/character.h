06e04d0b290d66782d0a13a26ecb9a37738b7b53
Colin Zeidler <c.zeidler.wc@gmail.com>
2015-02-15 12:58:40 -0500

    creating stuff for Characters

--- CommonLibrary/Headers/character.h
+++ CommonLibrary/Headers/character.h
@@ -1,11 +1,22 @@
 #ifndef CHARACTER_H
 #define CHARACTER_H
+
 #include "commonlibrary_global.h"
+#include "clearing.h"
+#include "characterAdvantages.h"
+#include "equipment.h"
+#include <vector>
 
 //Abstract class, extend this with all different Character types
 class  COMMONLIBRARY_EXPORT Character{
 public:
 	Character();
+
+private:
+	int gold;
+	Clearing *location;
+	CharAdvantages advantages[2];
+	std::vector<Equipment> *equipment;
 };
 
 #endif // CHARACTER_H
---
+++ CommonLibrary/Headers/equipment.h
@@ -0,0 +1,13 @@
+#ifndef EQUIPMENT_H
+#define EQUIPMENT_H
+
+#include "commonlibrary_global.h"
+
+class COMMONLIBRARY_EXPORT Equipment {
+public:
+	Equipment();
+private:
+	int value;
+}
+
+#endif // EQUIPMENT_H
