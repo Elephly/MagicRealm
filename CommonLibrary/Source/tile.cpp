d2345608910a2d5b24990c243cf8f6082d698ce6
Luke Harper <lukeharper@live.ca>
2015-02-19 21:07:05 -0500

    Got rid of the switch
    YEAH

--- CommonLibrary/Source/tile.cpp
+++ CommonLibrary/Source/tile.cpp
@@ -43,14 +43,6 @@ void Tile::addConnectedTile(Tile* newTile, Direction edge)
     int boardEdge = 0;
     int targetEdge = 0;
 
-    switch(boardEdge){
-        case 0:     targetEdge = 3; break;
-        case 1:     targetEdge = 4; break;
-        case 2:     targetEdge = 5; break;
-        case 3:     targetEdge = 0; break;
-        case 4:     targetEdge = 1; break;
-        case 5:     targetEdge = 2; break;
-    }
     for(int i =0; i<CONNECTED_LENGTH; i++){
         //when we hit null we have iterated over all current adjacent tiles
         if(connectedTiles[i] == NULL){
@@ -60,7 +52,7 @@ void Tile::addConnectedTile(Tile* newTile, Direction edge)
             boardEdge = (edge+orientation) % 6;
             for(int i=0; i<CONNECTED_LENGTH; i++){
                 oppEdge = (i+newTile->getOrientation()) % 6;
-                if(oppEdge == targetEdge){
+                if(oppEdge == ((boardEdge +3) % 6)){
                     newTile->addConnectedTile(this, (Direction) oppEdge);
                     return;
                 }
