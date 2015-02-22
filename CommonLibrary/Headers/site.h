55ab17c8a7902c0bff4b9cb8d7077abdbe1dd18a
Luke Harper <lukeharper@live.ca>
2015-02-22 13:47:27 -0500

    Added Treasure to the sites
    access the site by looting returns null if not at the position,
    otherwise treasure pointer

--- CommonLibrary/Headers/site.h
+++ CommonLibrary/Headers/site.h
@@ -1,18 +1,30 @@
 #ifndef SITE_H
 #define SITE_H
 
+#include <vector>
+
 #include "shared.h"
 #include "commonlibrary_global.h"
 
 #include "chit.h"
+#include "treasure.h"
 
 class COMMONLIBRARY_EXPORT Site: public Chit {
 public:
-    Site(string n, bool hide, int clearing);
+    Site(string n, bool hide, int clearing, vector<Treasure*> * myTreasure);
+    ~Site();
     int getClearingNum();
+    /*
+    *   loot
+    *   in:         array index of treasure location
+    *   out:        Treasure that you got
+    *   Purpose:    Removes the treasure from the list
+    */
+    Treasure* loot(int location);
 
 protected:
     int clearingLocation;
+    vector <Treasure*> * trove;
 };
 
 #endif //SITE_H
--- CommonLibrary/Source/site.cpp
+++ CommonLibrary/Source/site.cpp
@@ -1,11 +1,29 @@
 #include "site.h"
 
-Site::Site(string n, bool hide, int clearing): Chit(n, hide)
+Site::Site(string n, bool hide, int clearing, vector<Treasure*> * myTreasure): Chit(n, hide)
 {
     clearingLocation = clearing;
+    trove = myTreasure;
+}
+
+Site::~Site()
+{
+    trove->clear();
+    delete trove;
+    trove = NULL;
 }
 
 int Site::getClearingNum()
 {
     return clearingLocation;
+}
+
+Treasure* Site::loot(int location)
+{
+    if(location >= trove->size())
+        return NULL;
+
+    Treasure* looted = trove->at(location);
+    trove->erase(trove->begin() + location);
+    return looted;
 }
